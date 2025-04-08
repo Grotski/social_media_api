806b7ea11f55b3832d0080492f3af59f8034c098
Misha <mishelgrotski9@gmail.com>
2025-04-07 22:22:08 -0600

    fix relations in models.py, add image_path feature for media

--- social_media_api_project/settings.py
+++ social_media_api_project/settings.py
@@ -126,11 +126,15 @@ USE_I18N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = "user.User"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media") 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -148,12 +152,12 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
-    "PAGE_SIZE": 100,
+    "PAGE_SIZE": 50,
 }
 
 SPECTACULAR_SETTINGS = {
-    "TITLE": "Airport Service API",
-    "DESCRIPTION": "Order tickets",
+    "TITLE": "Social Media Service API",
+    "DESCRIPTION": "Project for sharpening my skills in Django",
     "VERSION": "1.0.0",
     "SERVE_INCLUDE_SCHEMA": False,
     "SWAGGER_UI_SETTINGS": {
--- social_media_app/models.py
+++ social_media_app/models.py
@@ -1,30 +1,79 @@
+import pathlib
+import uuid
+
 from django.db import models
 from social_media_api_project import settings
 
 
-class Friends(models.Model):
-    followed = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="user"
+class Chat(models.Model):
+    sender = models.ForeignKey(
+        "Profile", on_delete=models.CASCADE, related_name="chats_sent"
     )
-    following = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="users"
+    receiver = models.ForeignKey(
+        "Profile", on_delete=models.CASCADE, related_name="chats_received"
+    )
+    content = models.TextField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+def image_path(instance, filename: str):
+    filename = f"{instance.id}-{uuid.uuid4()}" + pathlib.Path(filename).suffix
+    return pathlib.Path("uploads/media") / pathlib.Path(filename)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey("Post", on_delete=models.CASCADE, related_name="comments")
+    content = models.TextField(max_length=1000)
+    commenter = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="comments"
     )
     created_at = models.DateTimeField(auto_now_add=True)
-    
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.content}"
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    profile = models.ForeignKey(
+        "Profile", on_delete=models.CASCADE, related_name="posts"
+    )
+    content = models.TextField(max_length=1000)
+    media = models.ImageField(null=True, upload_to=image_path)
+    ceated_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title}: {self.content}"
+
+    class Meta:
+        ordering = ["title"]
+
+
+class Friends(models.Model):
+    profile = models.ForeignKey(
+        "Profile", on_delete=models.CASCADE, related_name="friends"
+    )
+    followed = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="followers"
+    )
+    following = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="followings"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
     class Meta:
         ordering = ["-created_at"]
 
 
 class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     date_of_bith = models.DateField(auto_now_add=True)
     bio = models.TextField(max_length=1000)
-    friends = models.ManyToManyField(
-        "Friends", blank=True, related_name="profiles"
-    )
-    posts = models.ForeignKey("Post", on_delete=models.CASCADE, related_name="profiles")
-    chats = models.ForeignKey("Chat", on_delete=models.CASCADE, related_name="profiles")
+    profile_picture = models.ImageField(null=True, upload_to=image_path)
 
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
